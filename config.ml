70f79ebb6313ec1b4d037218fdc859717506a476
Bramford Horton <bram.horton@gmail.com>
2017-11-14 10:03:24 +1300

    Fix for ocaml 4.X and mirage 3.X

--- config.ml
+++ config.ml
@@ -3,33 +3,42 @@ open Mirage
 let get_exn name fn =
   let res = Sys.getenv name in
   Printf.printf "\027[33mENV\027[m         %s => %s\n%!" name res;
-  fn (String.lowercase res)
-let get name ~default fn = try get_exn name fn with Not_found -> default
+  fn (String.lowercase_ascii res)
+
+let get name ~default fn =
+  try get_exn name fn
+  with Not_found -> default
+
 let ip name default =
   let fn = Ipaddr.V4.of_string_exn in
   get name ~default:(fn default) fn
 
-let main =
-  foreign "Dispatch.Main" (console @-> kv_ro @-> http @-> clock @-> job)
+let mask name default =
+  let fn = Ipaddr.V4.Prefix.of_string_exn in
+  get name ~default:(fn default) fn
 
 let ipv4_config =
-  let address  = ip "IP" "10.0.0.10" in
-  let netmask  = ip "NM" "255.255.255.0" in
-  let gateways = [ip "GW" "10.0.0.1"] in
-  { address; netmask; gateways }
+  let network  = (mask "NM" "10.0.0.0/24", ip "IP" "10.0.0.10") in
+  let gateway = Some (ip "GW" "10.0.0.1") in
+  { network; gateway }
 
-let stack console = direct_stackv4_with_static_ipv4 console tap0 ipv4_config
+let stack = static_ipv4_stack default_network ~config:ipv4_config
 
 let server =
-  conduit_direct (stack default_console)
+  conduit_direct stack
 
 let http_srv = http_server server
 let data = crunch "static"
 
-let () =
-  add_to_opam_packages ["magic-mime"];
-  add_to_ocamlfind_libraries ["re.str";"str";"magic-mime"];
+let main =
+  let packages = [
+    package "str"; package "magic-mime"; package "re.str"
+  ] in
+  foreign
+    ~packages
+    "Dispatch.Main" (console @-> kv_ro @-> http @-> pclock @-> job)
 
+let () =
   register "www" [
-    main $ default_console $ data $ http_srv $ default_clock
+    main $ default_console $ data $ http_srv $ default_posix_clock
   ]
